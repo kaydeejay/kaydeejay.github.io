a9edc62e31a519f3c39fb4d6793a9a7ecbd64ade
Kevin Devine-Jones <kevin.jones215@gmail.com>
2020-01-22 12:50:46 -0500

    add modal functionality to portfolio pictures

--- bin/portfolio.js
+++ bin/portfolio.js
@@ -1,14 +1,17 @@
 const portImages = document.querySelectorAll(".width-wrapper");
 
 portImages.forEach((currentEl) => {
+    currentEl.setAttribute("data-toggle", "modal");
+    currentEl.setAttribute("data-target", "#portfolioModal");
     if (currentEl.getAttribute('data-link') === ""){
         currentEl.style.display = "none";
-    } else {
-        currentEl.addEventListener('click', () => {
-                const url = currentEl.getAttribute('data-link');
-                const win = window.open(url, '_blank');
-                win.focus();
-            }
-        );
-    }
+    } 
+    // else {
+    //     currentEl.addEventListener('click', () => {
+    //             const url = currentEl.getAttribute('data-link');
+    //             const win = window.open(url, '_blank');
+    //             win.focus();
+    //         }
+    //     );
+    // }
 });
