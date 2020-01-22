9fa7602f1c8286ca33bfbe4b7fa0abbb4f4657db
Kevin Devine-Jones <kevin.jones215@gmail.com>
2020-01-22 17:00:38 -0500

    add populate modal function for portfolio

--- bin/portfolio.js
+++ bin/portfolio.js
@@ -5,7 +5,7 @@ const modalDetails = [
         name: "random-password",
         title: "Random Password Generator",
         description: "Generate a random, secure password with any combination of uppercase, lowercase, special, and numerical characters.",
-        screenshot: "",
+        screenshot: "../images/portfolio-images/portfolio1-1.jpg",
         repoURL: "https://github.com/kaydeejay/random-password-generator",
         pagesURL: "https://kaydeejay.github.io/random-password-generator/"
     },
@@ -13,7 +13,7 @@ const modalDetails = [
         name: "quiz-game",
         title: "Lord of the Rings Quiz Game",
         description: "Test your knowledge of Lord of the Rings Characters within the allotted time. The user is scored based on how long it takes them to finish the quiz, and incorrect answers subtract from your time remaining.",
-        screenshot: "",
+        screenshot: "../images/portfolio-images/portfolio1-2.jpg",
         repoURL: "https://github.com/kaydeejay/quiz-challenge-game",
         pagesURL: "https://kaydeejay.github.io/quiz-challenge-game/"
     },
@@ -21,7 +21,7 @@ const modalDetails = [
         name: "dnd-char-gen",
         title: "Dungeons & Dragons 5th Edition Character Generator",
         description: "Quickly and easily generate a player character for D&D. This application uses the Open5e API.",
-        screenshot: "",
+        screenshot: "../images/portfolio-images/portfolio1-3.jpg",
         repoURL: "https://github.com/kaydeejay/dnd-char-gen",
         pagesURL: "https://kaydeejay.github.io/dnd-char-gen/"
     },
@@ -29,24 +29,46 @@ const modalDetails = [
         name: "weather",
         title: "Weather Dashboard",
         description: "Find weather information for a city, including current weather, UV Index, and 5-day forecast. This application uses the OpenWeatherMap API.",
-        screenshot: "",
+        screenshot: "../images/portfolio-images/portfolio2-1.jpg",
         repoURL: "https://github.com/kaydeejay/weather-dashboard",
         pagesURL: "https://kaydeejay.github.io/weather-dashboard/"
     }
 ];
+
 
 portImages.forEach((currentEl) => {
     currentEl.setAttribute("data-toggle", "modal");
     currentEl.setAttribute("data-target", "#portfolioModal");
     if (currentEl.getAttribute('data-link') === ""){
         currentEl.style.display = "none";
-    } 
-    // else {
-    //     currentEl.addEventListener('click', () => {
-    //             const url = currentEl.getAttribute('data-link');
-    //             const win = window.open(url, '_blank');
-    //             win.focus();
-    //         }
-    //     );
-    // }
+    } else {
+        currentEl.addEventListener('click', populateModal);
+    }
 });
+        
+function populateModal(){
+    const selectedName = this.getAttribute('data-name');
+    const modalTitle = document.getElementById("portfolioModalTitle");
+    const modalImage = document.createElement("img");
+    const modalDesc = document.getElementsByClassName("modal-body");
+    const deployedBtn = document.getElementById("linkDeployed");
+    const repoBtn = document.getElementById("linkRepo");
+    modalDetails.forEach((currentEl) => {
+        if (currentEl.name === selectedName){
+            modalTitle.innerHTML = currentEl.title;
+            modalImage.setAttribute("src", currentEl.screenshot);
+            modalDesc.textContent = currentEl.description;
+            deployedBtn.addEventListener('click', () => {
+                const url = currentEl.pagesURL;
+                const win = window.open(url, 'blank');
+                win.focus();
+            });
+            repoBtn.addEventListener('click', () => {
+                const url = currentEl.repoURL;
+                const win = window.open(url, 'blank');
+                win.focus();
+            });
+        }
+    });
+}
+        
