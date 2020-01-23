47e16d995fa27e84b542e126fef716e178893674
Kevin Devine-Jones <kevin.jones215@gmail.com>
2020-01-22 19:23:15 -0500

    tweaks to modal image styling

--- bin/portfolio.js
+++ bin/portfolio.js
@@ -5,7 +5,7 @@ const modalDetails = [
         name: "random-password",
         title: "Random Password Generator",
         description: "Generate a random, secure password with any combination of uppercase, lowercase, special, and numerical characters.",
-        screenshot: "../images/portfolio-images/portfolio1-1.jpg",
+        screenshot: "./assets/images/portfolio-images/portfolio1-1.jpg",
         repoURL: "https://github.com/kaydeejay/random-password-generator",
         pagesURL: "https://kaydeejay.github.io/random-password-generator/"
     },
@@ -13,7 +13,7 @@ const modalDetails = [
         name: "quiz-game",
         title: "Lord of the Rings Quiz Game",
         description: "Test your knowledge of Lord of the Rings Characters within the allotted time. The user is scored based on how long it takes them to finish the quiz, and incorrect answers subtract from your time remaining.",
-        screenshot: "../images/portfolio-images/portfolio1-2.jpg",
+        screenshot: "./assets/images/portfolio-images/portfolio1-2.jpg",
         repoURL: "https://github.com/kaydeejay/quiz-challenge-game",
         pagesURL: "https://kaydeejay.github.io/quiz-challenge-game/"
     },
@@ -21,7 +21,7 @@ const modalDetails = [
         name: "dnd-char-gen",
         title: "Dungeons & Dragons 5th Edition Character Generator",
         description: "Quickly and easily generate a player character for D&D. This application uses the Open5e API.",
-        screenshot: "../images/portfolio-images/portfolio1-3.jpg",
+        screenshot: "./assets/images/portfolio-images/portfolio1-3.jpg",
         repoURL: "https://github.com/kaydeejay/dnd-char-gen",
         pagesURL: "https://kaydeejay.github.io/dnd-char-gen/"
     },
@@ -29,7 +29,7 @@ const modalDetails = [
         name: "weather",
         title: "Weather Dashboard",
         description: "Find weather information for a city, including current weather, UV Index, and 5-day forecast. This application uses the OpenWeatherMap API.",
-        screenshot: "../images/portfolio-images/portfolio2-1.jpg",
+        screenshot: "./assets/images/portfolio-images/portfolio2-1.jpg",
         repoURL: "https://github.com/kaydeejay/weather-dashboard",
         pagesURL: "https://kaydeejay.github.io/weather-dashboard/"
     }
