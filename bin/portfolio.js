26ca44c53f7a5ba13a6e7fcebf24e4ef26e3f9a9
Kevin Devine-Jones <kevin.jones215@gmail.com>
2020-03-02 02:35:10 -0500

    changes portfolio logic

--- bin/portfolio.js
+++ bin/portfolio.js
@@ -1,78 +1,103 @@
 const portImages = document.querySelectorAll(".width-wrapper");
 
-const modalDetails = [
-    {
-        name: "random-password",
-        title: "Random Password Generator",
-        description: "Generate a random, secure password with any combination of uppercase, lowercase, special, and numerical characters.",
-        screenshot: "./assets/images/portfolio-images/portfolio1-1.jpg",
-        repoURL: "https://github.com/kaydeejay/random-password-generator",
-        pagesURL: "https://kaydeejay.github.io/random-password-generator/",
-        herokuURL: null
-    },
-    {
-        name: "quiz-game",
-        title: "Lord of the Rings Quiz Game",
-        description: "Test your knowledge of Lord of the Rings Characters within the allotted time. The user is scored based on how long it takes them to finish the quiz, and incorrect answers subtract from your time remaining.",
-        screenshot: "./assets/images/portfolio-images/portfolio1-2.jpg",
-        repoURL: "https://github.com/kaydeejay/quiz-challenge-game",
-        pagesURL: "https://kaydeejay.github.io/quiz-challenge-game/",
-        herokuURL: null
-    },
-    {
-        name: "dnd-char-gen",
-        title: "Dungeons & Dragons 5th Edition Character Generator",
-        description: "Quickly and easily generate a player character for D&D. This application uses the Open5e API.",
-        screenshot: "./assets/images/portfolio-images/portfolio1-3.jpg",
-        repoURL: "https://github.com/kaydeejay/dnd-char-gen",
-        pagesURL: "https://kaydeejay.github.io/dnd-char-gen/",
-        herokuURL: null
-    },
-    {
-        name: "weather",
-        title: "Weather Dashboard",
-        description: "Find weather information for a city, including current weather, UV Index, and 5-day forecast. This application uses the OpenWeatherMap API.",
-        screenshot: "./assets/images/portfolio-images/portfolio2-1.jpg",
-        repoURL: "https://github.com/kaydeejay/weather-dashboard",
-        pagesURL: "https://kaydeejay.github.io/weather-dashboard/",
-        herokuURL: null
-    }
+const projects = [
+  {
+    name: 'dnd-char-gen',
+    title: 'Dungeons & Dragons Character Generator',
+    description: 'Quickly and easily create a character for D&D 5th Edition',
+    screenshot: './assets/images/portfolio-images/dnd-char-gen.jpg',
+    repoURL: 'https://github.com/kaydeejay/dnd-char-gen/',
+    deployedURL: 'https://kaydeejay.github.io/dnd-char-gen/'
+  },
+  {
+    name: 'weather-dashboard',
+    title: 'Weather Dashboard',
+    description: 'View the current 5-day forecast from a search by city. Powered by the openweathermap.org API.',
+    screenshot: './assets/images/portfolio-images/weather-dashboard.jpg',
+    repoURL: 'https://github.com/kaydeejay/weather-dashboard',
+    deployedURL: 'https://kaydeejay.github.io/weather-dashboard/'
+  },
+  {
+    name: 'burger-logger',
+    title: 'Burger Logger',
+    description: 'Record burgers you want to eat and have eaten. Uses a persistent MySQL database and a custom ORM.',
+    screenshot: './assets/images/portfolio-images/burger-logger.jpg',
+    repoURL: 'https://github.com/kaydeejay/burger-logger',
+    deployedURL: 'https://shrouded-island-52694.herokuapp.com/'
+  },
+  {
+    name: 'employee-tracker',
+    title: 'Employee Tracker',
+    description: 'A Node CLI content management system that allows the user to create, read, update, and delete entries in an employee roster.',
+    screenshot: './assets/images/portfolio-images/employee-tracker.jpg',
+    repoURL: 'https://github.com/kaydeejay/employee-tracker',
+    deployedURL: null
+  },
+  {
+    name: 'team-roster-gen',
+    title: 'Team Roster Generator',
+    description: 'A Node CLI application that generates a graphical project team roster based on user inputs.',
+    screenshot: './assets/images/portfolio-images/team-roster-gen.jpg',
+    repoURL: 'https://github.com/kaydeejay/team-roster-gen',
+    deployedURL: null
+  }
 ];
 
-
-portImages.forEach((currentEl) => {
-    currentEl.setAttribute("data-toggle", "modal");
-    currentEl.setAttribute("data-target", "#portfolioModal");
-    if (currentEl.getAttribute('data-link') === ""){
-        currentEl.style.display = "none";
+const makeProjects = () => {
+  portImages.forEach((el) => {
+    const thisIndex = el.getAttribute('data-index');
+    const thisProject = projects[thisIndex];
+  
+    el.setAttribute("data-toggle", "modal");
+    el.setAttribute("data-target", "#portfolioModal");
+  
+    if (!thisProject) {
+      el.style.display = "none";
     } else {
-        currentEl.addEventListener('click', populateModal);
+      // make the image and caption
+      const img = document.createElement('img');
+      const caption = document.createElement('p');
+      img.setAttribute('src', thisProject.screenshot);
+      img.setAttribute('class', 'img-fluid');
+      caption.textContent = thisProject.title;
+      caption.setAttribute('class', 'img-caption');
+      el.appendChild(img);
+      el.appendChild(caption);
+      el.addEventListener('click', populateModal);
     }
-});
-        
-function populateModal(){
-    const selectedName = this.getAttribute('data-name');
-    const modalTitle = document.getElementById("portfolioModalTitle");
-    const modalImage = document.getElementById("modalImg");
-    const modalDesc = document.getElementById("modalDesc");
-    const deployedBtn = document.getElementById("linkDeployed");
-    const repoBtn = document.getElementById("linkRepo");
-    modalDetails.forEach((currentEl) => {
-        if (currentEl.name === selectedName){
-            modalTitle.textContent = currentEl.title;
-            modalImage.setAttribute("src", currentEl.screenshot);
-            modalDesc.textContent = currentEl.description;
-            deployedBtn.addEventListener('click', () => {
-                const url = currentEl.pagesURL;
-                const win = window.open(url, 'blank');
-                win.focus();
-            });
-            repoBtn.addEventListener('click', () => {
-                const url = currentEl.repoURL;
-                const win = window.open(url, 'blank');
-                win.focus();
-            });
-        }
-    });
+  });
 }
-        
+
+function populateModal(){
+  const project = projects[this.getAttribute('data-index')];
+  const modalFooter = document.getElementById('modalFooter');
+  const modalTitle = document.getElementById('portfolioModalTitle');
+  const modalImg = document.getElementById('modalImg');
+  const modalDesc = document.getElementById('modalDesc');
+  const repoBtn = document.createElement('button');
+
+  modalFooter.innerHTML = '';
+
+  repoBtn.setAttribute('class','btn');
+  repoBtn.textContent = 'Visit Repository';
+  repoBtn.addEventListener('click', () => {
+    const win = window.open(project.repoURL, 'blank');
+    win.focus();
+  });
+  if (project.deployedURL){
+    const deployedBtn = document.createElement('button');
+    deployedBtn.setAttribute('class', 'btn');
+    deployedBtn.textContent = 'Deployed Application';
+    deployedBtn.addEventListener('click', () => {
+      const win = window.open(project.deployedURL, 'blank');
+      win.focus();
+    });
+    modalFooter.appendChild(deployedBtn);
+  }
+  modalTitle.textContent = project.title;
+  modalImg.setAttribute('src', project.screenshot);
+  modalDesc.textContent = project.description;
+  modalFooter.appendChild(repoBtn);
+}
+
+document.body.onload = makeProjects;
