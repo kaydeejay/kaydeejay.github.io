cb87cfdaa7a029fa848eb0e66bca652bf277b933
Kevin Devine-Jones <kevin.jones215@gmail.com>
2020-03-01 19:03:08 -0500

    adds gitignore, removes twitter link

--- bin/portfolio.js
+++ bin/portfolio.js
@@ -7,7 +7,8 @@ const modalDetails = [
         description: "Generate a random, secure password with any combination of uppercase, lowercase, special, and numerical characters.",
         screenshot: "./assets/images/portfolio-images/portfolio1-1.jpg",
         repoURL: "https://github.com/kaydeejay/random-password-generator",
-        pagesURL: "https://kaydeejay.github.io/random-password-generator/"
+        pagesURL: "https://kaydeejay.github.io/random-password-generator/",
+        herokuURL: null
     },
     {
         name: "quiz-game",
@@ -15,7 +16,8 @@ const modalDetails = [
         description: "Test your knowledge of Lord of the Rings Characters within the allotted time. The user is scored based on how long it takes them to finish the quiz, and incorrect answers subtract from your time remaining.",
         screenshot: "./assets/images/portfolio-images/portfolio1-2.jpg",
         repoURL: "https://github.com/kaydeejay/quiz-challenge-game",
-        pagesURL: "https://kaydeejay.github.io/quiz-challenge-game/"
+        pagesURL: "https://kaydeejay.github.io/quiz-challenge-game/",
+        herokuURL: null
     },
     {
         name: "dnd-char-gen",
@@ -23,7 +25,8 @@ const modalDetails = [
         description: "Quickly and easily generate a player character for D&D. This application uses the Open5e API.",
         screenshot: "./assets/images/portfolio-images/portfolio1-3.jpg",
         repoURL: "https://github.com/kaydeejay/dnd-char-gen",
-        pagesURL: "https://kaydeejay.github.io/dnd-char-gen/"
+        pagesURL: "https://kaydeejay.github.io/dnd-char-gen/",
+        herokuURL: null
     },
     {
         name: "weather",
@@ -31,7 +34,8 @@ const modalDetails = [
         description: "Find weather information for a city, including current weather, UV Index, and 5-day forecast. This application uses the OpenWeatherMap API.",
         screenshot: "./assets/images/portfolio-images/portfolio2-1.jpg",
         repoURL: "https://github.com/kaydeejay/weather-dashboard",
-        pagesURL: "https://kaydeejay.github.io/weather-dashboard/"
+        pagesURL: "https://kaydeejay.github.io/weather-dashboard/",
+        herokuURL: null
     }
 ];
 
